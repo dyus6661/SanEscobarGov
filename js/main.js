4fd0578255fdca1d2031543df1a0c75e96ac31be
Eduard <dyus6661@gmail.com>
2020-10-10 00:07:33 +0300

    Uploaded project files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('menu'),
+        'padding': 256,
+        'tolerance': 70
+    });
+
+    // Toggle button
+    document.querySelector('.toggle-button').addEventListener('click', function () {
+        slideout.toggle();
+    });
+
+    new WOW({
+        animateClass: "animate__animated"
+    }).init();
+});
